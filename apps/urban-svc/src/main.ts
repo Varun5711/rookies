cea4b93c6041daad276a4458b1c46bf1a8fa57af
Varun5711 <varunhotani@gmail.com>
2026-01-17 12:58:50 +0530

    chore:sette up microservices

--- apps/urban-svc/src/main.ts
+++ apps/urban-svc/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT_API_GATEWAY || process.env.PORT || 3000;
+  const port = process.env.PORT_URBAN_SVC || process.env.PORT || 3012;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
